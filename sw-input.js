59f4ef8e20ee76799e18039bdf47477c19c355b6
lborgman <lennart.borgman@gmail.com>
2025-03-23 22:10:04 +0100

    Exclude all .json files from workbox precaching

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.003";
+const SW_VERSION = "0.1.006";
 
 
 // https://www.npmjs.com/package/workbox-sw
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{html,svg,js,json}'
+		// '**/*.{html,svg,js,json}',
+		'**/*.{html,svg,js}',
+		// '!manifest-mm4i.json', // Exclude manifest-mm4i.json
 	],
 	swSrc: 'sw-input.js',
 	swDest: 'sw-workbox.js',
