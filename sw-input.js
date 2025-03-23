72b23f396cbf62afcafbad4ccc7adf80e8951ec3
lborgman <lennart.borgman@gmail.com>
2025-03-23 21:13:24 +0100

    Renamed js/TEMP dir to js/TEST

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.5.0000";
+const SW_VERSION = "0.1.0001";
 
 
 // https://www.npmjs.com/package/workbox-sw
