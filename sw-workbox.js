4133042795f68b163b273c66f9349fa8236a3e44
lborgman <lennart.borgman@gmail.com>
2025-03-23 21:29:10 +0100

    Change sw version to try to sync

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.0002";
+const SW_VERSION = "0.1.002";
 
 
 // https://www.npmjs.com/package/workbox-sw
