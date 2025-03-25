e2f000facc4a6ca2731e8fab8b66068703db8700
lborgman <lennart.borgman@gmail.com>
2025-03-25 15:14:31 +0100

    Make mm4i.svg shape a circle

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.0k";
+const SW_VERSION = "0.1.0l";
 
 
 // https://www.npmjs.com/package/workbox-sw
