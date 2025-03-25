db1b912be28d129137de9bbeac52a6fbc41de731
lborgman <lennart.borgman@gmail.com>
2025-03-25 23:08:01 +0100

    Copy the new icon to the default icon

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.0s";
+const SW_VERSION = "0.1.0t";
 
 
 // https://www.npmjs.com/package/workbox-sw
