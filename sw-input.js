118177185eb5d4f5fca5c56880218d798cb8b439
lborgman <lennart.borgman@gmail.com>
2025-03-25 22:16:50 +0100

    Make manifest icon midnightblue

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.0p";
+const SW_VERSION = "0.1.0q";
 
 
 // https://www.npmjs.com/package/workbox-sw
