9758420912ab04818d6ea8bc2f8d8f023e1490de
lborgman <lennart.borgman@gmail.com>
2025-03-25 21:35:08 +0100

    Rename the icon + more visible change to icon

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.0o";
+const SW_VERSION = "0.1.0p";
 
 
 // https://www.npmjs.com/package/workbox-sw
