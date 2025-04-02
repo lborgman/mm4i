30f1dfb33529e3265b4eb67d9351fb3995079f48
lborgman <lennart.borgman@gmail.com>
2025-04-02 17:50:29 +0200

    Another try to get rid of webpack

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.22";
+const SW_VERSION = "0.1.23";
 
 
 // https://www.npmjs.com/package/workbox-sw
