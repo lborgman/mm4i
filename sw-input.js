9e32a9bcda794b8e353e8adefa5d2d9c81dc9315
lborgman <lennart.borgman@gmail.com>
2025-04-02 17:40:44 +0200

    Remove webpack from package.json

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.21";
+const SW_VERSION = "0.1.22";
 
 
 // https://www.npmjs.com/package/workbox-sw
