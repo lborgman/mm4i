ccfcc9751015df5570ad4e5c382fd74dc154c801
lborgman <lennart.borgman@gmail.com>
2025-05-04 10:49:23 +0200

    Bump version number

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.31";
+const SW_VERSION = "0.1.32";
 
 
 // https://www.npmjs.com/package/workbox-sw
