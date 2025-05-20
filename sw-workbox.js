be28ce8ef4939c94c8998ce2294930e64b9aed87
lborgman <lennart.borgman@gmail.com>
2025-05-20 09:32:54 +0200

    Add more symbols to woff2 file

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.75";
+const SW_VERSION = "0.1.76";
 
 
 // https://www.npmjs.com/package/workbox-sw
