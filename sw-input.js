99a3572f1a497f7b842fe6734f3c6845e4299239
lborgman <lennart.borgman@gmail.com>
2025-05-20 11:16:01 +0200

    Add .woff2 to cached files

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.1.77";
+const SW_VERSION = "0.1.78";
 
 
 // https://www.npmjs.com/package/workbox-sw
