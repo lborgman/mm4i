43cce5c32e8c5bb3676beeedadb9c0a2113b52b1
lborgman <lennart.borgman@gmail.com>
2025-08-26 12:41:36 +0200

    Bug expander: just a check

--- js/jsmind-edit-common.js
+++ js/jsmind-edit-common.js
@@ -1955,7 +1955,7 @@ export async function pageSetup() {
         // doExpanding();
         setTimeout(doExpanding, 1000);
         async function doExpanding() {
-            alert("Bug hunting: click was on expander"); // expanding works with alert here
+            alert("Bug hunting: in timeout click was on expander"); // expanding works with alert here
             // modMdc.mkMDCdialogAlert("Bug hunting: click was on expander"); // 
             await modTools.waitSeconds(1);
             alert("Bug hunting: .waitSeconds(1)"); // expanding works with alert here
