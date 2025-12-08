e7c2a1639e457fb11ecaf07eb2dd13b6f6ce1583
lborgman <lennart.borgman@gmail.com>
2025-12-08 16:07:19 +0100

    Bump version number

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 // @ts-ignore
-const SW_VERSION = "0.2.321-2"; // Changed version to verify new SW is running
+const SW_VERSION = "0.2.321-3"; // Changed version to verify new SW is running
 
 /**
  * @module sw-init
