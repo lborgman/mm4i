f1881ed279fc1a03fe4e2c2ecf6747524fecc3eb
lborgman <lennart.borgman@gmail.com>
2026-01-05 00:39:31 +0100

    Remove dialog for choosing delegated events

--- js/jsmind-edit-common.js
+++ js/jsmind-edit-common.js
@@ -1195,6 +1195,7 @@ export async function displayOurMindmap(mindStored) {
     ///// One of these!
     // if (modJsmindDraggable.setJmnodeDragged) {
     let strChoosen = await (async () => {
+        return "connectFsm";
         if (location.hostname != "localhost") return "connectFsm";
         const mkChoice = (what) => {
             return mkElt("div", undefined, [
