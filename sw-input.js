5948c0753d825f63a3d81da3c4b2c11f9ad6d381
lborgman <lennart.borgman@gmail.com>
2026-02-06 02:16:04 +0100

    Update version number

--- sw-input.js
+++ sw-input.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 // @ts-ignore
-const SW_VERSION = "0.2.321-1004"; // Changed version to verify new SW is running
+const SW_VERSION = "0.2.321-1005"; // Changed version to verify new SW is running
 
 /**
  * @module sw-init
